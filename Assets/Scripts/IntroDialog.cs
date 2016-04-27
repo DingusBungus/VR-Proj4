1f77c87cedeba642248b932fb081b878634bc9fc
DingusBungus <themantimes8@aol.com>
2016-04-27 18:32:23 -0400

    Updated hospital scene

--- Assets/Scripts/IntroDialog.cs
+++ Assets/Scripts/IntroDialog.cs
@@ -7,56 +7,43 @@ public class IntroDialog : MonoBehaviour {
 	public Text text;
 	public int line = 0;
 	public int endAtLine = 0; //the last line of text.  If the button is pressed here, move to another scene.
-
-	public string[] ScriptLines = 
-	{
-		//(start up in village, Creator talks to you)
-
-		"Doctor, I don't feel so good"	//1
-
-		,"Uhhhhhhh..."	//2
-
-		,"Doctor: Take some pills"	//2
-
-		,"Thanks"	//3
-
-		,"Doctor: Only take them once a day"	//4
-
-		,"I feel much better now"	//5
-
-		,"Goodbye" //6
-
-
-	};
+	public TextAsset textFile;
+	public string[] textLines;
 
 	// Use this for initialization
 	void Start()
 	{
-		endAtLine = ScriptLines.Length-1;	//6
+		if (textFile != null) {
+			textLines = textFile.text.Split ('\n');
+		}
+		if (endAtLine == 0) {
+			endAtLine = textLines.Length - 1;
+		}
 	}
 
 	// Update is called once per frame
 	void Update()
 	{
-		text.text = ScriptLines [line];
+		text.text = textLines[line];
 
-		if (Input.GetKeyDown (KeyCode.P)) {
+		if (Input.GetKeyDown(KeyCode.JoystickButton2) || Input.GetKeyDown(KeyCode.P)) {
 			line++;
 		}
 
 		if (line > endAtLine) {
 			//Finish and move on to scene.
 			DisableTextBox();
+			//Application.LoadLevel("Hospital");
 		}
 	}
 
 	public void EnableTextBox()
 	{
-		textBox.SetActive (true);
+		textBox.SetActive(true);
 	}
 
 	public void DisableTextBox()
 	{
-		textBox.SetActive (false);
+		textBox.SetActive(false);
 	}
-	}
+}
